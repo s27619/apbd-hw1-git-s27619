d666423e4c6764dc339cc7b585ec233fac4aa784
Roman Klimovich <s27619@pjwstk.edu.pl>
2026-03-09 14:48:22 +0100

    Add basic console message for calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Calculator");
+Console.WriteLine("Enter numbers separated by space:");
