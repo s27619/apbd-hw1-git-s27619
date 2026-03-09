bd49db016829992d0f38b6b48fd8a93c04237b23
Roman Klimovich <s27619@pjwstk.edu.pl>
2026-03-09 15:09:50 +0100

    Add validation for CalculateAverage method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,7 +1,12 @@
 public static class StatisticsHelper
-{
+{	
 	public static double CalculateAverage(int[] values)
 	{
+		if (values.Length == 0)
+		{
+			throw new ArgumentException("Array cannot be empty");
+		}
+
 		return values.Average();
 	}
 }
