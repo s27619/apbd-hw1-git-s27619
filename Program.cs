2357f3384a22d65b94a1881a591876ac59b59534
Roman Klimovich <s27619@pjwstk.edu.pl>
2026-03-09 14:49:58 +0100

    Add basic user input handling

--- Program.cs
+++ Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Calculator");
 Console.WriteLine("Enter numbers separated by space:");
+
+string input = Console.ReadLine();
+string[] dividedParts = input.Split(' ');
